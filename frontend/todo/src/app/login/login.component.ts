0940791fbfe919543b9a429d7a40644be25ed3fd
Hector Duenas <hectorduenas.hd@gmail.com>
2020-06-28 20:16:52 -0700

    Added routing component and configured routing between login and welcome page.

--- frontend/todo/src/app/login/login.component.ts
+++ frontend/todo/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username = 'hector'
+  password= ''
+  errorMessage = 'Invalid Credentials'
+  invalidLogin = false
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+
+  handleLogin(){
+    if(this.username === 'hector' && this.password === 'pwd'){
+      this.invalidLogin = false
+      this.router.navigate(['welcome'])
+    }else{
+      this.invalidLogin = true
+    }
+  }
 }
