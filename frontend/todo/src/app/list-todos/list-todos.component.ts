1027e43908e97faefaf85dc1b151306b72753ff4
Hector Duenas <hectorduenas.hd@gmail.com>
2020-07-13 23:16:15 -0700

    Added todo create component

--- frontend/todo/src/app/list-todos/list-todos.component.ts
+++ frontend/todo/src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoDataService } from './../service/data/todo-data.service'
+import { Router } from '@angular/router';
 
 export class Todo{
   constructor(
@@ -23,7 +24,8 @@ export class ListTodosComponent implements OnInit {
   message: string
 
   constructor(
-    private todoService:TodoDataService
+    private todoService:TodoDataService,
+    private router:Router
   ) { }
 
   ngOnInit() {
@@ -48,5 +50,10 @@ export class ListTodosComponent implements OnInit {
         this.refreshTodos();
       }
     );
+  }
+
+  updateTodo(id){
+    console.log(`update ${id}`);
+    this.router.navigate(['todos', id])
   }
 }
